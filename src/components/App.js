f11ffd1ccc01ed4ccd6622cd2beac5f14eb30c0b
pri65 <priyankasoniii65@gmail.com>
2023-01-14 13:18:54 +0530

    local storage

--- src/components/App.js
+++ src/components/App.js
@@ -19,24 +19,38 @@ function App() {
         </html>
       `)
     }, 250)
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
   return (
     <>
       <div className="pane top-pane">
-        <Editor 
-        language="xml" 
-        displayName="HTml" 
-        value={} 
-        onChange={} />
-        <Editor />
-        <Editor />
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="pane">
         <iframe
-        title="output"
-        sandbox="allow-scripts"
-        frameBorder="0"
-        width="100%"
-        height="100%"
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
         />
       </div>
     </>
