7ab16e18265b19531c4c4d0d3a0d2515dd379088
pri65 <priyankasoniii65@gmail.com>
2023-01-14 12:58:10 +0530

    Structure page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Editor from "./Editor";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        />
+      </div>
     </div>
   );
 }
