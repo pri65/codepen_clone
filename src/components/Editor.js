be953ee2ff0e9d5a229189d91c54bb1666f0c28e
pri65 <priyankasoniii65@gmail.com>
2023-01-14 13:14:41 +0530

    Editor file

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -8,38 +8,45 @@ import { Controlled as ControlledEditor } from 'react-codemirror2'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Editor = (props) => {
+
+
+export default function Editor(props) {
     const {
-        language,
-        displayName,
-        value,
-        onChange
+      language,
+      displayName,
+      value,
+      onChange
     } = props
-
-function handleChange(editor, data, value) {
-onChange(value)
-}
-
-  return (
-    <div className='editor-container'>
-     <div className='editor-title'>
-        {displayName}
-        <button>O/C</button>
-     </div>
-     <ControlledEditor
-     onBeforeChange={handleChange}
-     value={value}
-     className="code-mirror-wrapper"
-     options={{
-        lineWrapping: true,
-        lint: true,
-        mode: language,
-        theme:material,
-        lineNumbers: true
-     }}
-     />
-    </div>
-  )
-}
-
-export default Editor;
+    const [open, setOpen] = useState(true)
+  
+    function handleChange(editor, data, value) {
+      onChange(value)
+    }
+  
+    return (
+      <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+        <div className="editor-title">
+          {displayName}
+          <button
+            type="button"
+            className="expand-collapse-btn"
+            onClick={() => setOpen(prevOpen => !prevOpen)}
+          >
+            <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+          </button>
+        </div>
+        <ControlledEditor
+          onBeforeChange={handleChange}
+          value={value}
+          className="code-mirror-wrapper"
+          options={{
+            lineWrapping: true,
+            lint: true,
+            mode: language,
+            theme: 'material',
+            lineNumbers: true
+          }}
+        />
+      </div>
+    )
+  }
