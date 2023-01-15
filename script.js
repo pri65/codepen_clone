7fbc1764e8aefb8989263f48387ea834de519146
pri65 <priyankasoniii65@gmail.com>
2023-01-15 14:05:39 +0530

    set local storage

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const css_code = document.querySelector(".css-code textarea");
 const js_code = document.querySelector(".js-code textarea");
 
 const result = document.querySelector("#result");
+
+function run() {
+    localStorage.setItem('html_code', html_code.value);
+    result.contDocument.body.innerHtml = `<style>${css_code.value}</style>` + html_code.value; 
+}
+
+html_code.onkeyup = () => run();
+css_code.onkeyup = () => run();
+js_code.onkeyup = () => run();
