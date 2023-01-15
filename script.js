c43c8bbe3076df085bfff6b43b4c851c8fe2f058
pri65 <priyankasoniii65@gmail.com>
2023-01-15 14:22:00 +0530

    Project complete

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ const result = document.querySelector("#result");
 
 function run() {
     localStorage.setItem('html_code', html_code.value);
-    result.contDocument.body.innerHtml = `<style>${css_code.value}</style>` + html_code.value; 
+    localStorage.setItem('css_code', css_code.value);
+    localStorage.setItem('js_code', js_code.value);
+    result.contDocument.body.innerHtml = `<style>${localStorage.css_code}</style>` + localStorage.html_code; 
+    result.contentWindow.eval(localStorage.js_code)
 }
 
 html_code.onkeyup = () => run();
 css_code.onkeyup = () => run();
 js_code.onkeyup = () => run();
+
+html_code.value = localStorage.html_code;
+css_code.value = localStorage.css_code;
+js_code.value = localStorage.js_code;
