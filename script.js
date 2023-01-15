2dbe4fe2a18d1a95aa18ab887e38842fd0e6e3bb
pri65 <priyankasoniii65@gmail.com>
2023-01-15 14:00:40 +0530

    Creating js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const html_code = document.querySelector(".html-code textarea");
+const css_code = document.querySelector(".css-code textarea");
+const js_code = document.querySelector(".js-code textarea");
+
+const result = document.querySelector("#result");
